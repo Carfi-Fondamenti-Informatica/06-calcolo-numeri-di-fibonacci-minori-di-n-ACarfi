260172068a25a15db05c406772572799bd1cab00
ACarfi <carfi.alessandro@gmail.com>
2023-05-04 16:07:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    int a=1;
+    int b=1;
+    cin >> n;
+    cout << b << " " << b << " ";
+    while(true){
+        a += b;
+        int c = b;
+        b = a;
+        a = c;
+        cout << b << " ";
+        if(b > n){
+            break;
+        }
+    }
+    return 0;
 }
